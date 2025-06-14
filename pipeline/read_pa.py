91c1460675dcf47970f125711b2206bb42ca1c64
John <dejhswavyguy@gmail.com>
2025-06-14 14:23:31 -0500

    add requirements.txt

--- pipeline/read_pa.py
+++ pipeline/read_pa.py
@@ -1,6 +1,10 @@
 import pdfplumber
+import os
 
-with pdfplumber.open("../Input Data/Abdulla/referral_package.pdf") as pdf:
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+file_path = os.path.join(base_dir, "Input Data", "Adbulla", "PA.pdf")
+
+with pdfplumber.open(file_path) as pdf:
     first_page = pdf.pages[0]
     print(first_page.chars[0])
     
